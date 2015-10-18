ad823c0b119b73851b73b68943b25e3d91daafc2
Dan K <itsdanya@gmail.com>
2015-10-18 16:20:06 +0300

    Add helper methods to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,18 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def api_token
+    users(:foo).api_token
+  end
+
+  def json_response
+    JSON.parse(@response.body, symbolize_names: true)
+  end
 end
