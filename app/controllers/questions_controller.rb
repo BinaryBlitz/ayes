0416aaf4aec53a844626b53b2a39b1b78c3ffe7f
Dan K <itsdanya@gmail.com>
2015-11-09 11:21:08 +0300

    Fix questions index action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,6 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.urgent
     @questions += Schedule.for_now.includes(:question).map(&:question)
-    @questions += [PoolQuestion.next.question] if PoolQuestion.next
+    @questions += [PoolQuestion.next.question] if PoolQuestion.next.try(:question)
   end
 end
