896a8f7ab5d785f9ea3086226ac57f807ca5f64d
Dan K <itsdanya@gmail.com>
2015-11-16 16:51:42 +0300

    Add checkboxes for merge groups

--- app/controllers/admin/merge_groups_controller.rb
+++ app/controllers/admin/merge_groups_controller.rb
@@ -7,7 +7,23 @@ class Admin::MergeGroupsController < Admin::AdminController
     @merge_group = MergeGroup.new
   end
 
+  def create
+    @merge_group = MergeGroup.new(merge_group_params)
+
+    if @merge_group.save
+      redirect_to admin_merge_groups_path, notice: 'Группа слияния создана.'
+    else
+      render :new
+    end
+  end
+
   def destroy
+    @merge_group = MergeGroup.find(params[:id])
     @merge_group.destroy
+    redirect_to admin_merge_groups_path, notice: 'Группа слияния удалена.'
+  end
+
+  def merge_group_params
+    params.require(:merge_group).permit(:field, options: [])
   end
 end
