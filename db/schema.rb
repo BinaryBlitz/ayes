50726739327f75a6d32cfcfd6473053f4d29ade1
Dan K <itsdanya@gmail.com>
2015-11-18 00:16:09 +0300

    Add distribution fields to answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115201337) do
+ActiveRecord::Schema.define(version: 20151117210011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20151115201337) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "form_id"
+    t.integer  "yes_ratio"
+    t.integer  "total_count"
+    t.datetime "compared_at"
   end
 
   add_index "answers", ["form_id"], name: "index_answers_on_form_id", using: :btree
