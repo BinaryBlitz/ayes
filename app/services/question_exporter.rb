3c320c4e110dcf447560569bf2b61901a4b3d2fb
Dan K <itsdanya@gmail.com>
2015-11-24 17:18:36 +0300

    Add null count column to question exports

--- app/services/question_exporter.rb
+++ app/services/question_exporter.rb
@@ -3,13 +3,16 @@ class QuestionExporter
   def export
     CSV.generate(headers: true) do |csv|
       attributes = %w(id content region)
-      csv << attributes + ['yes', 'no', 'na', 'favorites', 'tags']
+      csv << attributes + ['yes', 'no', 'na', 'null', 'favorites', 'tags']
 
       Question.includes(:tags).find_each do |question|
+        null_count = User.count - question.answers.count
+        null_count = null_count < 0 ? 0 : null_count
         csv << question.attributes.values_at(*attributes) + [
           question.answers.positive.count,
           question.answers.negative.count,
           question.answers.neutral.count,
+          null_count,
           question.favorites.count,
           question.tags.map(&:name).join(', ')
         ]
